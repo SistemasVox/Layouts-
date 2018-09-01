88aded80677a605e36787b5121fe85bdfd61126d
Marcelo Vieira <dj.marcelo.2009@gmail.com>
2018-09-01 15:46:57 -0300

    Versão 1.0

---
+++ app/src/main/java/com/layouts/marcelo/layouts/CardapioActivity.java
@@ -0,0 +1,14 @@
+package com.layouts.marcelo.layouts;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CardapioActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardapio);
+    }
+
+}
---
+++ app/src/main/java/com/layouts/marcelo/layouts/GridActivity.java
@@ -0,0 +1,13 @@
+package com.layouts.marcelo.layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GridActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid);
+    }
+}
